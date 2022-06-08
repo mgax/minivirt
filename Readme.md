2705d4c9ff3545c50be5990515e17153834becdf
Alex Morega <alex@grep.ro>
2022-06-08 20:13:19 +0300

    Log in with SSH

--- Readme.md
+++ Readme.md
@@ -16,3 +16,32 @@ python3 -m venv .venv
 ./minivirt.sh console foo  # To exit the console, type ^].
 ./minivirt.sh kill foo
 ```
+
+### SSH
+
+Add these lines to your ssh config (`~/.ssh/config`):
+
+```ssh-config
+Host *.minivirt
+  Include ~/.cache/minivirt/*/ssh-config
+```
+
+Start the VM, then connect to its console, and log in as root:
+
+```shell
+./minivirt.sh start foo
+./minivirt.sh console foo
+```
+
+Inside the VM, set up an SSH server:
+
+```shell
+echo | setup-alpine -q
+setup-sshd -k https://github.com/{username}.keys openssh
+```
+
+Then exit the console with `^]` (control-]). Now you can connect to the VM using SSH:
+
+```shell
+ssh foo.minivirt
+```
--- minivirt/__main__.py
+++ minivirt/__main__.py
@@ -7,6 +7,8 @@ from time import time, sleep
 import socket
 import json
 import shutil
+import random
+from textwrap import dedent
 
 import click
 import daemon
@@ -86,13 +88,32 @@ class VM:
         self.vm_path = db.vm_path(name)
         self.qmp_path = self.vm_path / 'qmp'
         self.serial_path = self.vm_path / 'serial'
+        self.ssh_config_path = self.vm_path / 'ssh-config'
 
     def connect_qmp(self):
         return QMP(self.qmp_path)
 
     def start(self, image):
         logger.info('Starting %s ...', self.name)
+
         self.vm_path.mkdir(parents=True)
+        ssh_port = random.randrange(20000, 32000)
+        with self.ssh_config_path.open('w') as f:
+            f.write(
+                dedent(
+                    f'''\
+                        Host {self.name}.minivirt
+                            StrictHostKeyChecking no
+                            UserKnownHostsFile /dev/null
+                            Hostname localhost
+                            Port {ssh_port}
+                            User root
+                    '''
+                )
+            )
+
+        self.ssh_config_path.chmod(0o644)
+
         try:
             subprocess.check_call(
                 [
@@ -107,6 +128,8 @@ class VM:
                         f'if=pflash,format=raw,file={FIRMWARE},readonly=on'
                     ),
                     '-boot', 'menu=on,splash-time=0',
+                    '-netdev', f'user,id=user,hostfwd=tcp::{ssh_port}-:22',
+                    '-device', 'virtio-net-pci,netdev=user,romfile=',
                     '-device', 'virtio-gpu-pci',
                     '-display', 'default,show-cursor=on',
                     '-device', 'qemu-xhci',
