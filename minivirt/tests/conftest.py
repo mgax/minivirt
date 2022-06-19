09ff84a73a050d1814e6d437d727c6c830a9c3f1
Alex Morega <alex@grep.ro>
2022-06-19 09:07:23 +0300

    Wipe database before each testing session

--- minivirt/tests/conftest.py
+++ minivirt/tests/conftest.py
@@ -1,4 +1,5 @@
 import logging
+import shutil
 import subprocess
 from pathlib import Path
 
@@ -11,9 +12,16 @@ from minivirt.vms import VM
 logger = logging.getLogger(__name__)
 
 CACHE_PATH = Path.home() / '.cache' / 'minivirt-tests'
+DB_PATH = CACHE_PATH / 'db'
 BASE_IMAGE_URL = (
     f'https://f003.backblazeb2.com/file/minivirt/alpine-3.15.4-{arch}.tgz'
 )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def wipe_db():
+    if DB_PATH.exists():
+        shutil.rmtree(DB_PATH)
 
 
 @pytest.fixture
@@ -31,7 +39,7 @@ def base_image_path():
 
 @pytest.fixture
 def db(base_image_path):
-    db = DB(CACHE_PATH / 'db')
+    db = DB(DB_PATH)
 
     if not db.image_path('base').exists():
         with base_image_path.open('rb') as f:
