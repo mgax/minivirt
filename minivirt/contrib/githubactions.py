86d95eb3ac35822270a59b26c5bf18aa97339b39
Alex Morega <alex@grep.ro>
2022-09-30 22:56:20 +0300

    Execute ephemeral runner in ephemeral VM

--- minivirt/contrib/githubactions.py
+++ minivirt/contrib/githubactions.py
@@ -2,12 +2,13 @@ import json
 import logging
 import socket
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 from contextlib import closing
+from time import time
 
 import click
-import waitress
-from github import Github
-from pyngrok import ngrok
+
+from minivirt.vms import VM
 
 logger = logging.getLogger(__name__)
 
@@ -29,15 +30,57 @@ def fetch_github_token():
     return token
 
 
-def create_webhook(github_token, repo, url):
+def github_repo_api(token, repo):
+    from github import Github
+
+    return Github(token).get_repo(repo)
+
+
+def create_webhook(repo, url):
     config = dict(url=url, content_type='json')
-    events = ['ping']
-    g = Github(github_token)
-    repo = g.get_repo(repo)
+    events = ['ping', 'workflow_job']
     return repo.create_hook('web', config, events, active=True)
 
 
+def runner(github_repo):
+    from minivirt.cli import db
+    from minivirt.contrib.ci import GITHUB_RUNNER_URL
+
+    logger.info('Fetching runner registration token')
+    _, data = github_repo._requester.requestJsonAndCheck(
+        'POST', f'{github_repo.url}/actions/runners/registration-token'
+    )
+    registration_token = data['token']
+
+    vm_name = f'githubactions_{time()}'.replace('.', '_')
+    image = db.get_image('githubactions')
+    logger.info('Creating VM %s', vm_name)
+    vm = VM.create(db, vm_name, image=image, memory=512)
+    try:
+        with vm.run(wait_for_ssh=30):
+            vm.ssh(
+                # TODO move the runner installation to the image
+                f'mkdir actions-runner && cd actions-runner'
+                f' && curl -Ls {GITHUB_RUNNER_URL} | tar xz'
+                f' && ./bin/Runner.Listener configure'
+                f'      --url {github_repo.html_url}'
+                f'      --token {registration_token}'
+                f'      --ephemeral'
+                f'      --unattended'
+            )
+            logger.info('Starting runner')
+            vm.ssh(
+                'RUNNER_ALLOW_RUNASROOT=yes /root/actions-runner/run.sh'
+            )
+
+    finally:
+        vm.destroy()
+
+
 class Webhook:
+
+    def __init__(self, start_runner):
+        self.start_runner = start_runner
 
     def __call__(self, environ, start_response):
         def respond(status, data):
@@ -62,6 +105,14 @@ class Webhook:
         logger.info('Webhook ping: %s', payload['zen'])
         return 'pong'
 
+    def handle_workflow_job(self, payload):
+        job = payload['workflow_job']
+        action = payload['action']
+        logger.info('Webhook workflow job: %s %s', job['run_id'], action)
+        if action == 'queued':
+            self.start_runner()
+        return 'thanks'
+
 
 @click.group()
 def cli():
@@ -71,16 +122,24 @@ def cli():
 @cli.command()
 @click.argument('repo')
 def serve(repo):
-    github_token = fetch_github_token()
+    import waitress
+    from pyngrok import ngrok
+
+    github_repo = github_repo_api(fetch_github_token(), repo)
     port = find_free_port()
     listen = f'127.0.0.1:{port}'
     tunnel = ngrok.connect(port, bind_tls=True)
     logger.info('ngrok tunnel %s', tunnel.public_url)
 
-    webhook = create_webhook(github_token, repo, tunnel.public_url)
+    webhook = create_webhook(github_repo, tunnel.public_url)
 
     try:
-        waitress.serve(Webhook(), listen=listen)
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            def start_runner():
+                executor.submit(runner, github_repo)
+
+            wsgi_app = Webhook(start_runner)
+            waitress.serve(wsgi_app, listen=listen)
 
     finally:
         webhook.delete()
