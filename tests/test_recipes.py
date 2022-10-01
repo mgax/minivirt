4c990d99ea3222bedc4a688c7bdff9c01b7d25b5
Alex Morega <alex@grep.ro>
2022-10-01 11:05:23 +0300

    Skip `ubuntu-22.04` recipe

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -10,5 +10,7 @@ recipes = (Path(__file__).resolve().parent.parent / 'recipes')
 @pytest.mark.slow
 @pytest.mark.parametrize('path', list(recipes.glob('*.yaml')))
 def test_build_recipe(db, path):
+    if path.name == 'ubuntu-22.04.yaml':
+        pytest.skip(f'Recipe {path.name} is broken')
     image = build(db, path)
     assert image is not None
