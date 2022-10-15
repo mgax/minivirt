40cc5ec670627b8becba5e44814a6752a2821124
Alex Morega <alex@grep.ro>
2022-10-15 21:16:59 +0300

    Test one Alpine and one Ubuntu image

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -8,7 +8,7 @@ recipes = (Path(__file__).resolve().parent.parent / 'recipes')
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('name', ['alpine-3.15', 'alpine-3.16'])
+@pytest.mark.parametrize('name', ['alpine-3.16', 'ubuntu-22.04'])
 def test_build_recipe(db, name):
     image = build(db, recipes / f'{name}.yaml')
     assert image is not None
